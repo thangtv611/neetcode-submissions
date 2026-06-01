a82fa3ca3758b9fad5588547faa56c023f7028d8
Thang Tran Viet <thangtv611@gmail.com>
2026-06-01 16:52:16 +0700

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.js
@@ -0,0 +1,60 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        // brute forte
+
+        /* let found = false;
+        for (let i = 0; i < nums.length; i ++) {
+            for (let j = 0; j < nums.length; j ++) {
+                if (i !== j && nums[i] === nums[j]) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        return found; */
+
+        // set
+        /* const seen = new Set();
+        for (const item of nums) {
+            if (seen.has(item)) {
+                return true;
+            }
+            seen.add(item);
+        }
+
+        return false; */
+
+        // map
+        /* const counter = new Map();
+        for (const item of nums) {
+            counter.set(item, (counter.get(item) || 0) + 1);
+        }
+
+        for (const value of counter.values()) {
+            if (value > 1) {
+                return true;
+            }
+        }
+        return false; */
+
+        // hash-set length trick
+
+        /* const seen = new Set();
+        for (const item of nums) {
+            seen.add(item);
+        }
+        return seen.size < nums.length; */
+
+        // sorting
+        nums.sort();
+        for (let i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] === nums[i]) return true;
+        }
+        return false;
+    }
+}
